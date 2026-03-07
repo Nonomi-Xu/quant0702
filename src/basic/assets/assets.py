cfd4bc55c89e745f2f3c2e4c99a3d073ed86159d
Nonomi-Xu <xyy137782325@gmail.com>
2026-03-07 19:09:14 +0800

    更新每日数据库刷新、日刷新

--- src/basic/assets/assets.py
+++ src/basic/assets/assets.py
@@ -1,12 +1,11 @@
 # src/basic/assets.py
-from .data_ingestion.first_time.start_stock_list_duckdb import Start_Stock_List
-from .data_ingestion.first_time.start_stock_list_st_parquet import Start_Stock_List_ST
-from .data_ingestion.first_time.start_daily_price_parquet import Start_Daily_Prices
-from .data_ingestion.first_time.start_trade_cal_parquet import Start_Trade_Cal
+from .data_ingestion.start.start_stock_list_duckdb import Start_Stock_List
+from .data_ingestion.start.start_stock_list_st_parquet import Start_Stock_List_ST
+from .data_ingestion.start.start_daily_price_parquet import Start_Daily_Prices
+from .data_ingestion.start.start_trade_cal_parquet import Start_Trade_Cal
 
 from .data_ingestion.daily.daily_stock_list_duckdb import Daily_Stock_List
-from .data_ingestion.daily_org import *
-from .data_ingestion.daily_price import *
+
 from .data_ingestion.single_operation_price import *
 
 Data_Ingestion_Start_INFO_assets = [
